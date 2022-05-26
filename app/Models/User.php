27cb01d4bb8c02dc0b4abe00bdfac3f95062ffda
Rodrigo Chang <rodrigo.chang@gmail.com>
2022-05-26 04:11:15 -0400

    Modificaciones Dashboard
    Pantalla de creación de usuario con Roles estando con sesión de administración iniciada. Funcional.
    
    Posibilidad de edición de los usuarios solamente en su Rol, Nombre y Correo. 
    
    Arreglo de boton Eliminar habilitado para eliminar los usuarios ahora. 
    
    Creación nueva vista para creación y ordenes en el Controlador.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -41,6 +42,11 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
 
     public function roles()
     {
